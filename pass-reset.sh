9c7e5c57a9bdc6dfbcb7e207332a2623d1b2bb75
Dragon 117th <117thdragon@gmail.com>
2025-01-28 12:34:07 +0000

    1-st

---
+++ pass-reset.sh
@@ -0,0 +1,10 @@
+# Create or reset the openvpn administrative local account with specified password
+
+echo "Enter new password for user 'openvpn"
+read -s PASSWORD
+
+cd /usr/local/openvpn_as/scripts
+./sacli --user "openvpn" --key "prop_superuser" --value "true" UserPropPut
+./sacli --user "openvpn" --key "user_auth_type" --value "local" UserPropPut
+./sacli --user "openvpn" --new_pass=$PASSWORD SetLocalPassword
+./sacli start
