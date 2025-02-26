206e2b8cdd0b64477a035030c44a7a85986f0f45
Aleksey <167528592+117Dragon@users.noreply.github.com>
2025-02-26 15:17:59 +0400

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -2,7 +2,7 @@
 
 # Install OVPN Access Server
 sudo timedatectl set-timezone Europe/Moscow
-sudo apt update && sudo apt -y install ca-certificates wget net-tools gnupg mc ncdu fail2ban apt-transport-https ca-certificates nginx zip unzip
+sudo apt update && sudo apt -y install mc zip ncdu wget gnupg unzip nginx certbot fail2ban net-tools сa-certificates apt-transport-https  
 sudo mkdir -p /etc/apt/keyrings && sudo curl -fsSL https://packages.openvpn.net/as-repo-public.asc | sudo tee /etc/apt/keyrings/as-repository.asc
 sudo echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/as-repository.asc] http://packages.openvpn.net/as/debian jammy main" | sudo tee /etc/apt/sources.list.d/openvpn-as-repo.list
 sudo apt update && sudo apt -y install openvpn-as
