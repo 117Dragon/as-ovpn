905baff2f312c5db8bc9e0ca252ec1463663a942
Aleksey <167528592+117Dragon@users.noreply.github.com>
2025-02-27 10:44:02 +0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@
 sudo apt update && sudo apt install git -y
 cd /tmp/
 git clone https://github.com/117Dragon/as.git
-sudo sh ./as/scripts/install_dependencies.sh
+sudo bash ./as/scripts/install_dependencies.sh
